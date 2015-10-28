1952e0894356f182a3fe4df1dce2e325afbdea86
Jason Ibrahim <jibrahim@opentext.com>
2015-10-28 08:47:33 -0700

    can now download expense report to the device from content server

--- www/js/services/document.js
+++ www/js/services/document.js
@@ -19,11 +19,9 @@
 
         function downloadFile(fileId, filename, success, fail) {
             var downloadUrl = $auth.gatewayUrl() + '/content/v4/nodes/' + fileId + '/content?versionNum=1&cstoken=' + $auth.getOTCSTicket(),
-                options = {
-                    headers: {
-                        otcsticket: $auth.getOTCSTicket()
-                    }
-                };
+                options = new FileUploadOptions();
+
+            options.headers = {'otcsticket': $auth.getOTCSTicket()};
             $appworks.storage.storeFile(filename, downloadUrl, success, fail, options);
         }
 
