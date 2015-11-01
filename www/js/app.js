adf90db9afa4fcbf66c49652364e23a30dd8249a
Jason Ibrahim <jibrahim@opentext.com>
2015-11-01 12:07:04 -0800

    styling updates and removal of home tab in favor of all encompassing expeditions tab

--- www/js/app.js
+++ www/js/app.js
@@ -8,7 +8,7 @@ angular
 angular
     .module('scout')
     .run(function ($rootScope) {
-        $rootScope.version = '0.8.3';
+        $rootScope.version = '0.9.0';
     });
 
 angular
--- www/js/controllers/expeditions-controller.js
+++ www/js/controllers/expeditions-controller.js
@@ -2,18 +2,62 @@ angular
     .module('scout.controllers')
     .controller('ExpeditionsController', ExpeditionsController);
 
-function ExpeditionsController($scope, Expedition, $stateParams, $state) {
+function ExpeditionsController($scope, Expedition, $stateParams, $state, StockImage, $ionicModal) {
+
+    // variable bindings
+    $ionicModal.fromTemplateUrl('templates/expeditions/new-expedition.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.modal = modal;
+    });
+    $scope.image = StockImage.random();
     $scope.expeditions = Expedition.all();
     $scope.remove = function ($index) {
         $scope.expeditions.splice($index, 1);
     };
+    $scope.newExpedition = {};
 
+    // function bindings
     $scope.filterFn = filter;
     $scope.clearFilter = clearFilter;
     $scope.reload = reload;
     $scope.go = $state.go;
+    $scope.startNewExpedition = startNewExpedition;
+    $scope.openNewExpeditionModal = openNewExpeditionModal;
+    $scope.closeNewExpeditionModal = closeNewExpeditionModal;
+    $scope.reload = reload;
 
     setFilter();
+
+    function startNewExpedition(expedition) {
+        Expedition.create(expedition).then(function (newExpedition) {
+            $scope.newExpedition = {};
+            $scope.expeditions.push(newExpedition);
+            $scope.modal.hide();
+        });
+    }
+
+    function clearFilter() {
+        $scope.filter = null;
+        $stateParams.filter = '';
+        $scope.filterActive = false;
+    }
+
+    function closeNewExpeditionModal() {
+        $scope.modal.hide();
+    }
+
+    function filter(actual) {
+        if ($scope.filterActive) {
+            return actual.id === $scope.filter;
+        }
+        return true;
+    }
+
+    function openNewExpeditionModal() {
+        $scope.modal.show();
+    }
 
     function reload() {
         $scope.expeditions = Expedition.all();
@@ -25,18 +69,5 @@ function ExpeditionsController($scope, Expedition, $stateParams, $state) {
             $scope.filterActive = true;
             $scope.filter = $stateParams.filter;
         }
-    }
-
-    function clearFilter() {
-        $scope.filter = null;
-        $stateParams.filter = '';
-        $scope.filterActive = false;
-    }
-
-    function filter(actual) {
-        if ($scope.filterActive) {
-            return actual.id === $scope.filter;
-        }
-        return true;
     }
 }
