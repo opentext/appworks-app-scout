451e7a3d00bd3446c1a41f56f6ac2f4a48899b69
Jason Ibrahim <jibrahim@opentext.com>
2015-11-03 10:32:04 -0800

    showing expedition status next to title

--- www/js/app.js
+++ www/js/app.js
@@ -8,7 +8,7 @@ angular
 angular
     .module('scout')
     .run(function ($rootScope) {
-        $rootScope.version = '1.0.0';
+        $rootScope.version = '1.0.1';
         $rootScope.contentServicePath = '/content/v5/nodes/';
         $rootScope.scoutServicePath = '/scoutService/api/expeditions';
     });
@@ -26,7 +26,6 @@ function onIonicReady($ionicPlatform) {
         if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
             cordova.plugins.Keyboard.disableScroll(true);
-
         }
         if (window.StatusBar) {
             // org.apache.cordova.statusbar required
